01a25c52f971d8de29ce9e9960f33f132d2c7be0
龍馬 安部 <13004@hakodate.kosen-ac.jp>
2017-09-15 15:59:52 +0900

    アクセスの例追加

--- Src/procon2017/procon2017/draw.py
+++ Src/procon2017/procon2017/draw.py
@@ -4,4 +4,11 @@ class Draw:
 
     def __init__(self, child, pieces):
         print("test")
+        #例
+        now_node = child
+        for i in range(3):
+            print("piece:" + str(now_node.piece_n) + " next_edge:" + str(now_node.next_edge_n) + " prev_edge:" + str(now_node.prev_edge_n))
+            now_node = now_node.prev
+            
+
         #ここに描画の関数を書く
