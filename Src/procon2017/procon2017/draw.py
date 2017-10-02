7214677369972274e7849768f0c64ea2e271f24a
龍馬 安部 <13004@hakodate.kosen-ac.jp>
2017-10-02 16:12:32 +0900

    draw 仮完成

--- Src/procon2017/procon2017/draw.py
+++ Src/procon2017/procon2017/draw.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import Store 
 
 class draw:
     """description of class"""
@@ -20,29 +21,61 @@ class draw:
         for (i) in range(len(polygon)-1):
             piece_n = now_node.piece_n
             x = polygon[piece_n][now_node.prev_edge_n]
+            h = now_node.prev_edge_n##
+            prev_edge_n = now_node.prev_edge_n + 1
+            if prev_edge_n == len(polygon[piece_n]):
+                    prev_edge_n = 0
 
             now_node = now_node.prev
 
             if now_node.next_edge_n < total_edge[0]:
                 length = len(polygon[0])
                 next_edge_n = now_node.next_edge_n + 1
+                
                 if next_edge_n == length:
-                    next_edge_n = 0
-                y = polygon[0][next_edge_n] 
+                   next_edge_n = 0
+                
+                y = polygon[0][next_edge_n]
+                
+                y1 = polygon[0][now_node.next_edge_n]
+            
             elif now_node.next_edge_n >= total_edge[1]:
                 length = len(polygon[2])
                 next_edge_n = now_node.next_edge_n - total_edge[1] + 1
                 if next_edge_n == length:
                     next_edge_n = 0
                 y = polygon[2][next_edge_n] 
+                y1 = polygon[2][now_node.next_edge_n - total_edge[1]]
             else:
                 length = len(polygon[1])
                 next_edge_n = now_node.next_edge_n - total_edge[0] + 1
                 if next_edge_n == length:
                     next_edge_n = 0
-                y = polygon[1][now_node.next_edge_n]
+                y = polygon[1][next_edge_n]
+                y1 = polygon[1][now_node.next_edge_n - total_edge[0]]
 
             instance = y - x
+            for (i, piece) in enumerate(polygon[piece_n]):
+                polygon[piece_n][i] += instance
+
+            #角度計算
+            VecA = y1 - y
+            VecB = polygon[piece_n][prev_edge_n] - y
+            Ang = Store.cul_angle(VecA[0], VecB[0])
+
+            #軸を(0,0)に平行移動
+            instance = x - 0
+            for (i, piece) in enumerate(polygon[piece_n]):
+                polygon[piece_n][i] -= instance
+
+            for (i, piece) in enumerate(polygon[piece_n]):
+                # 元の行列
+                a = polygon[piece_n][i]
+                if i != h:
+                    # 回転による座標変換
+                    a = rotate(Ang, a)
+
+            #もとに戻す
             for (i, piece) in enumerate(polygon[piece_n]):
                 polygon[piece_n][i] += instance
 
@@ -53,6 +86,17 @@ class draw:
             cv2.imshow('image', img)
             cv2.waitKey(0)          
             cv2.destroyAllWindows
+
+def rotate(deg, matrix):
+    # degreeをradianに変換
+    r = np.radians(deg)
+    C = np.cos(r)
+    S = np.sin(r)
+    
+    matrix[0][0] = matrix[0][0] * C - matrix[0][1] * S
+    matrix[0][1] = matrix[0][0] * S + matrix[0][1] * C
+
+    return matrix
         
         
             
