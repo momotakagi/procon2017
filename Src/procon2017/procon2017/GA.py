0797f7ef13a0eb87f11640a99ec036a0474cfd05
詠政 布施 <xexixsxexix.mth@gmail.com>
2017-10-06 16:46:58 +0900

    GA もう少し

--- Src/procon2017/procon2017/GA.py
+++ Src/procon2017/procon2017/GA.py
@@ -69,10 +69,15 @@ class GA(object):
         Black = 0
         Total_pixels =0
 
+        Max_White = 0
+
         pieces = self.pieces.polygon
         pix = self.waku_data.pixels
         
-        for (i) in range(len(pieces)): 
+        for (n) in range(len(pieces)):
+            White = 0
+            Total_pixels =0
+            self.img = np.zeros((self.height, self.width, 3), np.uint8)
             #ピースバラマキ
             for (i) in range(len(pieces)):
             
@@ -107,24 +112,83 @@ class GA(object):
                 cv2.fillPoly(self.img, [pts], color=(255,255,255))
                 self.img = cv2.polylines(self.img,[pts],True,(0,255,0))  
 
-        #枠内での白黒確認
-        for i in range(len(pix)):
+            #枠内での白黒確認
+            for i in range(len(pix)):
             
-            self.pixelValue = self.img[pix[i][0][1],pix[i][0][0],1]
+                self.pixelValue = self.img[pix[i][0][1],pix[i][0][0],1]
 
-            Total_pixels += 1    
+                Total_pixels += 1    
             
-            if self.pixelValue == 255:
-                White += 1
-         
+                if self.pixelValue == 255:
+                    White += 1
+                
+           # print("White"+ str(n) +" = " + str(White))
+
+            if White > Max_White:
+                Max_White = White
+
+                suit_pix = pieces
+
+        for (n) in range (40):
             
+            for (i) in range(len(pieces)):
+
+                White = 0
+                Total_pixels =0
+                self.img = np.zeros((self.height, self.width, 3), np.uint8)
+            
+                #枠内のピクセル値をランダムで抽出
+                random_n = randint(len(pix))                                             
+                random_pixel = pix[random_n]
+            
+                #グリッド化  問題あるかも？
+                random_gred = random_pixel / [__RATIO_Y,__RATIO_X]                                          
+
+                #移動距離を計算
+                s = random_gred - (suit_pix[i][0] / [__RATIO_X,__RATIO_Y])                       
+            
+                #ピースの全頂点を移動
+                for (j) in range(len(pieces[i])):                                              
+           
+                    suit_pix[i][j] =  suit_pix[i][j] + s * [__RATIO_X,__RATIO_Y]
+            
+                #描画　表示はしていない
+                pts = np.array(suit_pix[i], np.int32)
+                pts = pts.reshape((-1,1,2))
+                cv2.fillPoly(self.img, [pts], color=(255,255,255))
+                self.img = cv2.polylines(self.img,[pts],True,(0,255,0))  
+
+            #枠内での白黒確認
+            for i in range(len(pix)):
+            
+                self.pixelValue = self.img[pix[i][0][1],pix[i][0][0],1]
+
+                Total_pixels += 1    
+            
+                if self.pixelValue == 255:
+                    White += 1
+                
+            #print("White"+ str(n) +" = " + str(White))
+
+            if White > Max_White:
+                Max_White = White
+
+                suit_pix = pieces
                     
-        Black = Total_pixels - White
+        Black = Total_pixels - Max_White
         
         print("Total =" + str(Total_pixels))
-        print("White =" + str(White))
+        #print("White =" + str(White))
         print("Black =" + str(Black))
         
+        print("Max_White =" + str(Max_White))
+
+        for (i) in range(len(pieces)):
+            pts = np.array(suit_pix[i], np.int32)
+            pts = pts.reshape((-1,1,2))
+            cv2.fillPoly(self.img, [pts], color=(255,255,255))
+            self.img = cv2.polylines(self.img,[pts],True,(0,255,0))  
+         
         cv2.imshow('image', self.img)         
         cv2.waitKey(0)
         cv2.destroyAllWindows
